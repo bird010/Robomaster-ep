18ac240fdf09a64f87e4a50adc2c5d2eb2244856
bird010 <128909505+bird010@users.noreply.github.com>
2025-01-31 12:14:42 +0800

    lock_person: chase detected person, stop while getting close, use gripper open and close.

--- lock_person.py
+++ lock_person.py
@@ -1,3 +1,5 @@
+# script to rotate the robomaster ep to keep detected person in the middle of the camera frame.
+
 import cv2
 from robomaster import robot
 from robomaster import camera
@@ -7,6 +9,42 @@ import numpy as np
 
 # Load YOLOv10 model
 model = YOLOv10.from_pretrained('jameslahm/yolov10n')
+
+class EpStatus:
+    def __init__(self):
+        self._front_dist = 0
+        self._grip_status = 'normal'
+
+    def set_front_dist(self, distance):
+        self._front_dist = distance
+
+    def get_front_dist(self):
+        return self._front_dist
+
+    def dist_sub_data_handler(self, sub_info):
+        distance = sub_info
+        self.set_front_dist(distance[0])
+        print("tof1:{0}".format(distance[0]))
+
+    def set_grip_status(self, status):
+        self._grip_status = status
+
+    def get_grip_status(self):
+        return self._grip_status
+    
+    def grip_open(self, ep_gripper):
+        if self.get_grip_status() != 'opened':
+            ep_gripper.open()
+
+    def grip_close(self, ep_gripper):
+        if self.get_grip_status() != 'closed':
+            ep_gripper.close()
+
+    def grip_sub_data_handler(self, sub_info):
+        # 完全闭合 closed, 完全张开opened, 处在中间位置normal.
+        status = sub_info
+        self.set_grip_status(status)
+        print("gripper status:{0}.".format(status))
 
 def detect_person(frame):
     # Convert frame to PIL Image
@@ -20,9 +58,15 @@ if __name__ == '__main__':
     ep_robot = robot.Robot()
     ep_robot.initialize(conn_type="sta")
 
+    ep_status = EpStatus()
+
     ep_camera = ep_robot.camera
     ep_chassis = ep_robot.chassis
-
+    ep_gripper = ep_robot.gripper
+    ep_gripper.sub_status(freq=5, callback=ep_status.grip_sub_data_handler)
+    ep_sensor = ep_robot.sensor
+    ep_sensor.sub_distance(freq=20, callback=ep_status.dist_sub_data_handler)
+    
     ep_camera.start_video_stream(display=True, resolution=camera.STREAM_360P)
 
     frame_width = 640
@@ -60,11 +104,27 @@ if __name__ == '__main__':
             # Calculate the error from the center
             error_x = float(x) - 0.5
             if abs(error_x) > 0.05:
-                # Rotate chassis to keep person in the center
-                ep_chassis.drive_speed(x=0, y=0, z=error_x * 500)
+                if(ep_status.get_front_dist() < 700):
+                    # Rotate only
+                    ep_chassis.drive_speed(x=0, y=0, z=error_x * 600)
+                    ep_status.grip_close(ep_gripper)
+                else:
+                    # Rotate and chase, to keep person in the center
+                    ep_chassis.drive_speed(x=0.5, y=0, z=error_x * 600)
+                    ep_status.grip_open(ep_gripper)
+
             else:
-                ep_chassis.drive_speed(x=0, y=0, z=0)
+                # person in the center
+                if(ep_status.get_front_dist() < 700):
+                    # stop
+                    ep_chassis.drive_speed(x=0, y=0, z=0)
+                    ep_status.grip_close(ep_gripper)
+                else:
+                    # Chase person
+                    ep_chassis.drive_speed(x=0.7, y=0, z=0, timeout=5)
+                    ep_status.grip_open(ep_gripper)
         else:
+            # Searching person
             ep_chassis.drive_speed(x=0, y=0, z=50)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -72,6 +132,8 @@ if __name__ == '__main__':
 
     cv2.destroyAllWindows()
     ep_camera.stop_video_stream()
+    ep_sensor.unsub_distance()
+    ep_gripper.unsub_status()
     ep_robot.close()
 
 
